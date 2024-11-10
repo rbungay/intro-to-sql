71d9c90adaf6c29e68c4b230929a1f117fb17da5
Rani Bungay <ranibungay@gmail.com>
2024-11-10 18:55:23 -0500

    complete exercise 5

--- clues.sql
+++ clues.sql
@@ -48,6 +48,14 @@ where countries.code = 'SMR';
 
 -- Write SQL query here
 
+SELECT cities.name, countries.name AS country_name
+FROM cities
+JOIN countries ON countries.code = cities.countrycode
+WHERE countries.continent = 'South America'
+AND cities.name ILIKE 'Serra%'  
+AND cities.name NOT ILIKE '%valle';   
+
+
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
